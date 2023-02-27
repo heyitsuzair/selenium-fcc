a09fdbe7eef74649c1e3829fa474b6ac2d59b831
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-02-28 00:00:28 +0500

    CSS Selector And Send Keys

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.common.keys import Keys
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 driver = webdriver.Chrome(options=options)
-driver.get('http://webdriveruniversity.com/Click-Buttons/index.html')
-driver.implicitly_wait(8)
-my_element = driver.find_element('id', 'button1')
-my_element.click()
+driver.get('https://webdriveruniversity.com/Contact-Us/contactus.html')
 
+fname = driver.find_element(By.NAME, 'first_name')
+lname = driver.find_element(By.NAME, 'last_name')
+email = driver.find_element(By.NAME, 'email')
+message = driver.find_element(By.NAME, 'message')
+reset = driver.find_elements(
+    By.CSS_SELECTOR, "input[class='contact_button']")[0]
+submit = driver.find_elements(
+    By.CSS_SELECTOR, "input[class='contact_button']")[1]
 
-WebDriverWait(driver, 30).until(
-    EC.text_to_be_present_in_element(
-        (By.CLASS_NAME, 'modal-title'),
-        'Congratulations!'
-    )
-)
+fname.send_keys('Muhammad')
+lname.send_keys('Uzair')
+email.send_keys('uzair@gmail.com')
+
+for i in range(3):
+    message.send_keys(Keys.SHIFT)
+    message.send_keys(Keys.ENTER)
+submit.click()
